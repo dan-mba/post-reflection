c4b9e8d065c7f30b20038537f883414fdd969623
Glitch (post-reflection) <none>
2025-06-18 12:47:26 +0000

    import

---
+++ routes/post.ts
@@ -0,0 +1,10 @@
+import {Express} from 'express';
+
+function get(app: Express) {
+  app.post('/', (request, response) => {
+    response.set('content-type', request.headers['content-type']);
+    response.send(request.body);
+  });
+};
+
+export default get;
